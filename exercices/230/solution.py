531e2ce1e6242e295d826357eb55168b192c3007
Agnes Koehler <nessi.is.da@googlemail.com>
2014-09-22 21:23:08 +0200

    corrections

--- exercices/230/solution.py
+++ exercices/230/solution.py
@@ -5,10 +5,9 @@ Created on Mon Sep 22 18:28:45 2014
 @author: Agnes
 """
 import is_prime
-var = 14
+var = 100000000
 b = True
 while b:
-    print(is_prime.is_prime(var))
     if is_prime.is_prime(var):
         b = False
     else:
