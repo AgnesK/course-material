5643c5a5c28df674d4bce778f941ec04d422f3de
Agnes Koehler <nessi.is.da@googlemail.com>
2014-09-22 14:43:38 +0200

    Exercises 000 to 060

--- exercices/019/solution.py
+++ exercices/019/solution.py
@@ -6,7 +6,7 @@ Created on Mon Sep 22 10:41:05 2014
 """
 import sys
 if len(sys.argv) < 3:
-    print("usage: python3 solution.py OP1 OP2")
+    print("python3 solution.py OP1 OP2")
 else:
     var = float(sys.argv[1]) + float(sys.argv[2])
     print(var)
